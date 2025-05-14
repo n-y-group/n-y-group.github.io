3ad83b1e307a0b67fe83011cce9a96f9d1073114
n-y-group <kirill_sergeevich1@mail.ru>
2025-05-14 20:39:46 +0500

    upload

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-const numSnowflakes = 300; // Количество снежинок
+document.getElementById('themeToggle').addEventListener('click', () => {
+  const html = document.documentElement;
+  const currentTheme = html.getAttribute('data-theme');
+  html.setAttribute('data-theme', currentTheme === 'light' ? 'dark' : 'light');
+});
 
-    for (let i = 0; i < numSnowflakes; i++) {
-      const snowflake = document.createElement('div');
-      snowflake.classList.add('snowflake');
-      snowflake.style.width = `${Math.random() * 10 + 5}px`; // случайный размер
-      snowflake.style.height = snowflake.style.width;
-      snowflake.style.left = `${Math.random() * 100}vw`; // случайная позиция по горизонтали
-      snowflake.style.animationDuration = `${Math.random() * 5 + 5}s`; // случайная продолжительность анимации
-      snowflake.style.animationDelay = `${Math.random() * 5}s`; // случайная задержка
+// Карусель изображений
+let carouselIndex = 0;
+const images = document.querySelectorAll('.carousel-image');
 
-      document.body.appendChild(snowflake);
+function showNextImage() {
+  images[carouselIndex].classList.remove('active');
+  carouselIndex = (carouselIndex + 1) % images.length;
+  images[carouselIndex].classList.add('active');
+}
 
-      // Для случайного движения снежинок
-      snowflake.style.animationName = 'fall';
-      snowflake.style.animationTimingFunction = 'linear';
-    }
-
-
+setInterval(showNextImage, 4000);
